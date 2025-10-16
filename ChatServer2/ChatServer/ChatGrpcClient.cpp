dee24d1ca306684b0a1058889a1846e2345bb0ff
SsXiao928 <2215045084@qq.com>
2025-10-16 16:51:03 +0800

    聊天服务器

---
+++ ChatServer2/ChatServer/ChatGrpcClient.cpp
@@ -0,0 +1,68 @@
+#include "ChatGrpcClient.h"
+#include "RedisMgr.h"
+#include "ConfigMgr.h"
+#include "UserMgr.h"
+
+#include "CSession.h"
+#include "MysqlMgr.h"
+
+ChatGrpcClient::ChatGrpcClient()
+{
+	auto& cfg = ConfigMgr::Inst();
+	auto server_list = cfg["PeerServer"]["Servers"];
+
+	std::vector<std::string> words;
+
+	std::stringstream ss(server_list);
+	std::string word;
+
+	while (std::getline(ss, word, ',')) {
+		words.push_back(word);
+	}
+
+	for (auto& word : words) {
+		if (cfg[word]["Name"].empty()) {
+			continue;
+		}
+		_pools[cfg[word]["Name"]] = std::make_unique<ChatConPool>(5, cfg[word]["Host"], cfg[word]["Port"]);
+	}
+
+}
+
+AddFriendRsp ChatGrpcClient::NotifyAddFriend(std::string server_ip, const AddFriendReq& req){
+	AddFriendRsp rsp;
+	Defer defer([&rsp, &req]() {
+		rsp.set_error(ErrorCodes::Success);
+		rsp.set_applyuid(req.applyuid());
+		rsp.set_touid(req.touid());
+		});
+
+	auto find_iter = _pools.find(server_ip);
+	if (find_iter == _pools.end()) {
+		return rsp;
+	}
+
+	auto& pool = find_iter->second;
+	ClientContext context;
+	auto stub = pool->getConnection();
+	Status status = stub->NotifyAddFriend(&context, req, &rsp);
+	Defer defercon([&stub, this, &pool]() {
+		pool->returnConnection(std::move(stub));
+		});
+	if (!status.ok()) {
+		rsp.set_error(ErrorCodes::RPCFailed);
+		return rsp;
+	}
+	return rsp;
+}
+AuthFriendRsp ChatGrpcClient::NotifyAuthFriend(std::string server_ip, const AuthFriendReq& req){
+	AuthFriendRsp rsp;
+	return rsp;
+}
+bool ChatGrpcClient::GetBaseInfo(std::string base_key, int uid, std::shared_ptr<UserInfo>& userinfo){
+	return true;
+}
+TextChatMsgRsp ChatGrpcClient::NotifyTextChatMsg(std::string server_ip, const TextChatMsgReq& req, const Json::Value& rtvalue){
+	TextChatMsgRsp rsp;
+	return rsp;
+}
